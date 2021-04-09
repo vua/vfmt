cd5a057b52cbd5389b9181e088112b50c7a8fdfc
vua <531218020@qq.com>
2021-04-09 15:54:49 +0800

    frist commit

--- test/vfmt_test.go
+++ test/vfmt_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"github.com/vua/vfmt"
 	"testing"
 )
@@ -13,18 +14,50 @@ type user struct {
 
 func Test(t *testing.T) {
 
-	//无样式
-	vfmt.Println("Console Output")
-	vfmt.Println("@[Console]@[ Output::]")
-	//无内容
-	vfmt.Println("@[::red]")
-	//有内容有样式
-	vfmt.Println("@[Console Output::red]")
-	//struct 输出
-	u:=user{1,"vua",18}
-	vfmt.Printf("@[%T,%v::#00ff00|bg#ff0000|bold]\n",u,u)
+	//有内容用样式
+	output := "Console Output"
+	style := "red"
+	vfmt.Printf("[vfmt] @[%s::%s]\n", output, style)
+	vfmt.Println("[vfmt] @[Console Output::red]")
+
+	//有内容无样式 (四者等价)
+	vfmt.Println("[vfmt] @[Console Output::]")
+	vfmt.Println("[vfmt] @[Console Output]")
+	vfmt.Println("[vfmt] Console Output")
+	fmt.Println("[vfmt] Console Output")
+
+	//有样式无内容 (五者等价)
+	vfmt.Println("[vfmt] @[::red]")
+	vfmt.Println("[vfmt] @[::]")
+	vfmt.Println("[vfmt] @[]")
+	vfmt.Println("[vfmt]")
+	fmt.Println("[vfmt]")
+
+	//结构体输出
+	/*
+		type user struct {
+			  id   int
+			  name string
+			  age  int
+		}
+	*/
+	u := user{1, "vua", 18}
+	vfmt.Printf("[vfmt] @[%T,%v::#00ff00|bg#ff0000|bold]\n", u, u)
+
+	//叠加样式
+	vfmt.Println("[vfmt] @[Console Output::green|bgRed|bold]")
+
+	//16进制颜色
+	vfmt.Println("[vfmt] @[Console Output::#00ff00|bg#ff0000|bold]")
+
 	//自定义样式
 	//创建样式：超链接
 	vfmt.RegisterStyle("url", "blue|underline")
-	vfmt.Println("@[https://www.github.com::url]")
+	vfmt.Println("[vfmt] @[https://www.github.com::url]")
+
+	//嵌套测试 (惰性匹配)
+	vfmt.Println("[vfmt] @[@[Console Output::yellow]::red]")
+
+	//并行测试 (惰性匹配)
+	vfmt.Println("[vfmt] @[Console Output::yellow]@[Console Output::#cc0ffe]")
 }
